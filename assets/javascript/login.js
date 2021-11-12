d60879f17bd99876318f81dfe356cff8997d20ee
Kevin Konczak <konczakkevin@gmail.com>
2021-11-12 16:03:01 +0900

    wrote JS script to login

---
+++ assets/javascript/login.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-button");
+const errorMessage = document.getElementById("error-message");
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+  if (username === "test" && password === "123456") {
+    window.location.replace('home.html');
+  } else {
+    errorMessage.style.display = block;
+  }
+})
